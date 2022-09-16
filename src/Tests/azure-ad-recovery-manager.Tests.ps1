dbdab09909dd4cf3e7cd45a4cb82fbacf08444cc
Harish <harish.karthic@gmail.com>
2022-09-16 12:28:52 +0100

    Updated tests

--- src/Tests/azure-ad-recovery-manager.Tests.ps1
+++ src/Tests/azure-ad-recovery-manager.Tests.ps1
@@ -6,9 +6,13 @@ Describe 'azure-ad-recovery-manager' {
 
     Context 'azure-ad-recovery-manager' {
         It 'Should test the end to end functionality of the module' {
+            if (!(Test-Path "$($PWD.Path)\Azure-AD-Backup_$(Get-Date -Format yyyyMMdd_HHmmss)")) {
+                $null = New-Item -Path "$($PWD.Path)\Azure-AD-Backup_$(Get-Date -Format yyyyMMdd_HHmmss)" -ItemType Directory
+            }
+            Set-BackupPath -FilePath "$($PWD.Path)\Azure-AD-Backup_$(Get-Date -Format yyyyMMdd_HHmmss)"
 
             if ($null -eq (Find-User -All)) {
-                $backup = Backup-AzADSecurityGroup -ShowOutput
+                $backup = Backup-AzADSecurityGroup -ShowOutput -AsJob -Verbose
                 $backup | Should -Not -Be $null
             }
 
